fa79e9bf5e0dc5d28d9dd37cacfa8b3a537c41c7
Madson C <madson@cardoso.org>
2018-09-17 21:15:53 -0700

    Introduces `cards` command. Just pass board/list names as params.

---
+++ cards.js
@@ -0,0 +1,115 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const CardsHelper = require('./lib/cardsHelper.js')
+const TrelloHelper = require('./lib/trelloHelper.js')
+const MathHelper = require('./lib/mathHelper.js')
+
+require('dotenv').config()
+
+program
+    .version('0.1.0')
+    .option('-b, --board "<boardName>"', 'Trello board name')
+    .option('-l, --list "<listName>"', 'Trello list name')
+    .option('-e, --export', 'Data to import to timesheet')
+    .option('-t, --time', 'Show PERT estimates')
+    .option('-a, --all', 'Show results for all members')
+    .parse(process.argv)
+
+if (program.board === undefined) {
+    console.log("Board name required.")
+    process.exit(1)
+}
+
+if (program.list === undefined) {
+    console.log("List name required.")
+    process.exit(1)
+}
+
+TrelloHelper.auth({
+    key: process.env.TRELLO_KEY,
+    token: process.env.TRELLO_TOKEN
+})
+
+let state = {}
+
+const cardsCallbackExport = (cards) => {
+    if (program.all) {
+        cards.forEach(card => {
+            CardsHelper.printCard(card)
+        })
+    } else {
+        cards.forEach(card => {
+            if (card.idMembers.includes(state.memberId)) {
+                CardsHelper.printCard(card)
+            }
+        })
+    }
+}
+
+const cardsCallbackTime = (cards) => {
+    let sum = 0
+    let optimistic = 0
+    let pessimistic = 0
+    let cardsCount = 0
+    let hours = 0
+    let name = ""
+    let matcher = null
+
+    const regex = /\[\s*(\d+)\S*\s*\S*\s*(\d+)\s*(\S*)\s*\]/s
+
+    cards.forEach(card => {
+        if (program.all === undefined) {
+            if (card.idMembers.includes(state.memberId) === false) { return }
+        }
+
+        cardsCount++
+        matcher = regex.exec(card.name)
+
+        if (matcher !== null) {
+            optimistic = parseFloat( matcher[1] )
+            pessimistic = parseFloat( matcher[2] )
+            pert = MathHelper.pert(optimistic, pessimistic)
+            sum += pert
+
+            hours = MathHelper.round2decimals(pert)
+            name = card.name.substring(0, 80)
+
+            console.log(hours + " ------ " + name);
+        } else {
+            console.log("Not matched - " + card.name);
+        }
+    })
+
+    console.log();
+    console.log("Number of cards: " + cardsCount);
+    console.log("Hours sum: " + MathHelper.round2decimals(sum));
+}
+
+const listCallback = (list) => {
+    if (program.export) {
+        TrelloHelper.getCards(list.id, cardsCallbackExport)
+    } else {
+        TrelloHelper.getCards(list.id, cardsCallbackTime)
+    }
+}
+
+const boardCallback = (board) => {
+    let listName = program.list
+    TrelloHelper.getLists(board.id, listName, listCallback)
+}
+
+const main = () => {
+    let boardName = program.board
+    TrelloHelper.getBoard(boardName, boardCallback)
+}
+
+if (program.all) {
+    main()
+} else {
+    TrelloHelper.getMe(user => {
+        console.log("Filtering results for: " + user.fullName + "\n");
+        state.memberId = user.id
+        main()
+    })
+}
